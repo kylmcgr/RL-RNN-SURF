8f5e3c44aeef80d01330c89789ffde09bd3d70a8
kmcgraw <kmcgraw@caltech.edu>
2021-08-10 13:02:32 -0700

    rl vs rnn pchoice plus rnn pred and label pred

--- src/EE/fit/rnn_pred.py
+++ src/EE/fit/rnn_pred.py
@@ -9,14 +9,14 @@ from actionflow.rnn.lstm_beh import LSTMBeh
 from actionflow.rnn.opt_beh import OptBEH
 from actionflow.util.helper import get_total_pionts
 from actionflow.util.logger import LogFile, DLogger
-from BD.data.data_reader import DataReader
-from BD.util.paths import Paths
+from EE.data.data_reader import DataReader
+from EE.util.paths import Paths
 import tensorflow as tf
 import pandas as pd
 
 cv_counts = 10
 cv_lists_group = {}
-data = DataReader.read_BD()
+data = DataReader.read_EE()
 for group in data['diag'].unique().tolist():
     gdata = data.loc[data.diag == group]
     ids = gdata['id'].unique().tolist()
@@ -33,23 +33,18 @@ for group in data['diag'].unique().tolist():
     cv_lists_group[group] = cv_lists
 
 configs = []
-for i in range(len(cv_lists_group['Healthy'])):
-    configs.append({'g': 'Healthy', 'lr': 0.01, 'cells': 10, 'cv_index': i, 'iters': 1100,
+for i in range(len(cv_lists_group['Novelty'])):
+    configs.append({'g': 'Novelty', 'lr': 0.01, 'cells': 10, 'cv_index': i, 'iters': 1100,
                     'model_path': '../inits/rnn-init/10cells/model-final/',
                     })
 
-for i in range(len(cv_lists_group['Depression'])):
-    configs.append({'g': 'Depression', 'lr': 0.01, 'cells': 10, 'cv_index': i, 'iters': 1200,
+for i in range(len(cv_lists_group['Uncertainty'])):
+    configs.append({'g': 'Uncertainty', 'lr': 0.01, 'cells': 10, 'cv_index': i, 'iters': 1200,
                     'model_path': '../inits/rnn-init/10cells/model-final/',
                     })
 
-for i in range(len(cv_lists_group['Bipolar'])):
-    configs.append({'g': 'Bipolar', 'lr': 0.01, 'cells': 20, 'cv_index': i, 'iters': 400,
-                    'model_path': '../inits/rnn-init/20cells/model-final/',
-                    })
 
-
-def run_BD_RNN(i):
+def run_EE_RNN(i):
     tf.reset_default_graph()
     ncells = configs[i]['cells']
     learning_rate = configs[i]['lr']
@@ -58,18 +53,18 @@ def run_BD_RNN(i):
     iters = configs[i]['iters']
     model_path = configs[i]['model_path']
 
-    output_path = Paths.local_path + 'BD/rnn-pred-diag/' + group + '/' + 'fold' + str(cv_index) + '/'
+    output_path = Paths.local_path + 'EE/rnn-pred-diag/' + group + '/' + 'fold' + str(cv_index) + '/'
     with LogFile(output_path, 'run.log'):
         indx_data = cv_lists_group[group][cv_index]
         gdata = data.loc[data.diag == group]
         train, test = DataProcess.train_test_between_subject(gdata, indx_data,
-                                                             [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])
+                                                             [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11])
 
         indx_data.to_csv(output_path + 'train_test.csv')
         train_merged = DataProcess.merge_data(train)
         DLogger.logger().debug("total points: " + str(get_total_pionts(train_merged)))
         del train
-        worker = LSTMBeh(2, 0, n_cells=ncells)
+        worker = LSTMBeh(2, 2, n_cells=ncells)
         OptBEH.optimise(worker, output_path, train_merged, None,
                         learning_rate=learning_rate, global_iters=iters, test_period=1e100,
                         load_model_path=model_path
@@ -92,4 +87,4 @@ if __name__ == '__main__':
     else:
         raise Exception('invalid argument')
 
-    run_cv(run_BD_RNN, n_proc)
+    run_cv(run_EE_RNN, n_proc)
--- src/EE/sim/rnn_label_pred.py
+++ src/EE/sim/rnn_label_pred.py
@@ -6,22 +6,21 @@ from os.path import isdir
 from actionflow.data.data_process import DataProcess
 from actionflow.rnn.lstm_beh import LSTMBeh
 from actionflow.util import DLogger
-from BD.data.data_reader import DataReader
+from EE.data.data_reader import DataReader
 import pandas as pd
-from BD.util.paths import Paths
+from EE.util.paths import Paths
 
 
 def finding_CV(base_input_folder):
     from os import listdir
     from os.path import join
 
-    group_address = {'Healthy': base_input_folder + 'Healthy/fold0/',
-                     'Bipolar': base_input_folder + 'Bipolar/fold0/',
-                     'Depression': base_input_folder + 'Depression/fold0/'}
+    group_address = {'Novelty': base_input_folder + 'Novelty/fold0/',
+                     'Uncertainty': base_input_folder + 'Uncertainty/fold0/'}
 
     subj_address = {}
 
-    for group in ['Healthy', 'Depression', 'Bipolar']:
+    for group in ['Novelty', 'Uncertainty']:
 
         input_folder = base_input_folder + group + '/'
         sims_dirs = [f for f in listdir(input_folder) if isdir(join(input_folder, f))]
@@ -41,7 +40,7 @@ def finding_CV(base_input_folder):
             if len(tst_ids) > 0:
                 _id = tst_ids.iloc[0]
                 subj_address[_id] = {}
-                for group2 in ['Healthy', 'Depression', 'Bipolar']:
+                for group2 in ['Novelty', 'Uncertainty']:
                     if group == group2:
                         subj_address[_id][group] = full_path
                     else:
@@ -51,12 +50,12 @@ def finding_CV(base_input_folder):
 
 
 def RNN_classify_subjects():
-    data = DataReader.read_BD()
+    data = DataReader.read_EE()
     ids = data['id'].unique().tolist()
     dftr = pd.DataFrame({'id': ids, 'train': 'train'})
     train, test = DataProcess.train_test_between_subject(data, dftr,
-                                                         [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12])
-    n_cells = {'Healthy': 10, 'Depression': 10, 'Bipolar': 20}
+                                                         [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11])
+    n_cells = {'Novelty': 10, 'Uncertainty': 10}
     model_iter = 'model-final'
 
     df = pd.DataFrame(columns=('model', 'id', 'loss'))
@@ -64,13 +63,12 @@ def RNN_classify_subjects():
 
     subj_paths = finding_CV(Paths.rest_path + 'archive/beh/rnn-pred-diag/')
 
-    for k, tr in train.iteritems():
-        for g, p in subj_paths[k].iteritems():
+    for k, tr in train.items():
+        for g, p in subj_paths[k].items():
             tf.reset_default_graph()
-            worker = LSTMBeh(2, 0, n_cells[g])
+            worker = LSTMBeh(2, 2, n_cells[g])
             saver = tf.train.Saver(max_to_keep=5)
-
-            DLogger.logger().debug('subject ' + k + ' group ' + g + ' path ' + p)
+            DLogger.logger().debug('subject ' + str(k) + ' group ' + g + ' path ' + p)
             model_path = p + model_iter + '/'
             ckpt = tf.train.get_checkpoint_state(model_path)
             tf.train.import_meta_graph(model_path + 'model.cptk.meta')
@@ -87,7 +85,7 @@ def RNN_classify_subjects():
 
                 df.loc[len(df)] = [g, k, total_loss]
 
-    df.to_csv(Paths.local_path + 'BD/rnn_diag.csv')
+    df.to_csv(Paths.local_path + 'EE/rnn_diag.csv')
 
 
 if __name__ == '__main__':
